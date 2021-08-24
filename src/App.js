3b55e46bb8ab4d72dab7ce106af1ad4c0293c679
jinhyeok <ekdrn3330@naver.com>
2021-08-24 17:37:38 +0900

    210824start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+/* eslint-disable */
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '서초 중국음식 맛집']);
+  let [따봉, 따봉변경] = useState(0);
+  let posts = '강남 고기 맛집';
+
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = '여자코트 추천';
+    글제목변경( newArray );
+  }
+  function 글자순정렬() {
+    var newArray = [...글제목];
+    newArray.sort();
+    글제목변경( newArray );
+  }
+  function 원래대로() {
+    글제목변경(['남자 코트 추천', '강남 우동 맛집', '서초 중국음식 맛집']);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      <button onClick={ 제목바꾸기 }>버튼</button>
+      <button onClick={ 글자순정렬 }>글자순 정렬</button>
+      <button onClick={ 원래대로 }>원래대로 돌리기</button>
+      <div className="list">
+        <h3> { 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 ++); } }>👍</span> { 따봉 } </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> { 글제목[1] } </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h3> { 글제목[2] } </h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
     </div>
   );
 }
